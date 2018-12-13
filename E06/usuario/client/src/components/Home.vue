518bdbba10df700cb0b1a5e549bebfc10f783877
usuario <usuario@debian.iaas.ull.es>
2018-12-13 12:37:10 +0000

    Borro otra vez

--- E06/usuario/client/src/components/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class="container">
-		<div class="jumbotron mt-5 ">
-			<h1 class="text-center">WELCOME</h1>
-			<img src="../img/consolas_284323.jpg" class="img-fluid" alt="Responsive image">
-		</div>
-	</div>
-</template>
