5448cab7b1dca793aa070ac33f0b0063e915270e
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-06-18 00:46:56 +0100

    Cambiada la configuración para generar el directorio /dist en /server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,37 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require("path")
 
 const app = express();
-mongoose.connect('mongodb://localhost/mevn-proyecto', {useNewUrlParser: true})
+
+app.use(express.static(path.join(__dirname, '/dist')))
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist'));
+})
+
+mongoose.connect('mongodb://localhost/mevn-proyecto', {
+		useNewUrlParser: true
+	})
 	.then(db => console.log('DB is connected'))
 	.catch(err => console.error(err));
 
 //Settings
-app.set ('port', process.env.PORT || 5000); 
+app.set('port', process.env.PORT || 5000);
 
 //Middlewares
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
 //Routes
 app.use('/users', require('./routes/users.js'));
+
+
 
 //Static files
 
