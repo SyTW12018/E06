b65bc96e7bc57701e6f4f6f398ea406e3cc97f8e
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-07-03 03:44:13 +0100

    Primera prueba (de prueba)

---
+++ client/test/index-spect.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect
+
+let User = require('../../server/models/User.js');
+let server = require('../../server/server.js');
+
+// const server = require('../../server/index.js');
+console.log("TEST TDD");
+
+describe('Prueba base', () => {
+  it('should return a string', () => {
+    expect('ci with travis').to.equal('ci with travis');
+  });
+});
