e2df0fe923fa5ecdd5d46d28c841f032fa602619
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-07-03 23:58:50 +0100

    Pasa juego y usuarios #6

--- client/test/juegos-test.js
+++ client/test/juegos-test.js
@@ -12,34 +12,16 @@ chai.use(chaiHttp);
 
 describe('Juegos', () => {
 
-
-  describe(' GET Juego por ID', () => {
-    it('Debería obtener un solo juego', (done) => {
-      var juego = new Juego({
-        _id: "5d1c13d566698839d4791a45",
-        titulo: "Fornite",
+  describe('GET Juegos', function () {
+    this.timeout(0);
+    it('Debería obtener todos los juegos', (done) => {
+      let juego = new Juego({
+        titulo: "Bioshock",
         categoria: "acción",
         fecha: "2007",
+        imagen: "imagen.png"
       });
-
-      chai.request(server)
-        .get('/juegos/ficha/' + juego.id)
-        .send(juego)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          res.body.should.have.property('titulo');
-          res.body.should.have.property('categoria');
-          res.body.should.have.property('fecha');
-          res.body.should.have.property('_id').eql(juego.id);
-          done();
-        });
-    });
-  });
-
-  describe('GET Juegos', () => {
-    it('Debería obtener todos los juegos', (done) => {
-
+      juego.save();
       chai.request(server)
         .get('/juegos')
         .end((err, res) => {
